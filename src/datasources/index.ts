88bf7e66d3cd118747165b84a2dc52c2dfc2115e
qmotas <1974745+qmotas@users.noreply.github.com>
2023-02-17 18:29:22 +0900

    消し忘れ

--- src/datasources/index.ts
+++
@@ -1,6 +0,0 @@
-import { Context } from "./../types/context";
-import { TrackAPI } from "./track-api";
-
-export const dataSources: () => Context["dataSources"] = () => ({
-  trackAPI: new TrackAPI(),
-});
